2aee3f9e4b8fbb7acac39abb010c40ba9e051ca4
O Famosoo Pedro <108557012+JoaopedrinCruzDm@users.noreply.github.com>
2024-07-31 11:49:27 -0300

    Add files via upload
    commitando arquivos de contador numérico usando sistema de loop

---
+++ main.kt
@@ -0,0 +1,12 @@
+fun main() {   //inicio
+    //println("Belo numero né?")
+  println("Esta aqui oque você pediu")
+  
+    for (i in 0..20678654577) { //vai mostrar o numero de 0 até o número 
+      
+        
+      print(" $i")
+ //vai mostrar valor
+    }
+ 
+}
---
+++ ok.kt
@@ -0,0 +1,5 @@
+//fun main (){
+//println("digite")
+//var n1 = readLine()!!.Toint()
+//if (n1 in 9..10)println("A")
+//}
